b780e9329dbf8800a8dd1da3bbf2ac9a40e7ea2b
none159 <noneenone019@gmail.com>
2023-10-22 00:18:42 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import About from './components/aboutpage'
 import Musicpage from './components/musicpage'
 
 import Loginpage from './components/loginpage'
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-  <>
-   <Router>
-          
-          <Routes>
-              
-              <Route path='./' element={<Page />} />    
-              <Route path='./music' element={<Musicpage />}/>
-              <Route path='./About' element={<About />}/>
-              <Route path='./login' element={<Loginpage />}/>
-             
-                       
-          </Routes>  
-            
-          
-        </Router> 
+    <>
+      <Router>
 
-   </>
+        <Routes>
+
+          <Route path='./musifyt' element={<Page />} />
+          <Route path='./music' element={<Musicpage />} />
+          <Route path='./About' element={<About />} />
+          <Route path='./login' element={<Loginpage />} />
+
+
+        </Routes>
+
+
+      </Router>
+
+    </>
   );
 }
 
